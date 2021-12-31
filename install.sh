7d78d53525e990ef32a5164e614415783b06ebe2
Francesco Carmelo Capria <leeenzyt@gmail.com>
2021-12-31 10:54:56 +0100

    Added an install script.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir ~/.fonts
+
+cp -r alacritty ~/.config/
+cp -r dunst ~/.config/
+cp -r emacs.d ~/
+cp -r i3 ~/.config/
+cp -r lock.sh ~/.config/
+cp -r neofetch ~/.config/
+cp -r picom ~/.config/
+cp -r polybar ~/.config/
+cp -r rofi ~/.config/
+cp AppleColorEmoji.ttf ~/.fonts/
+cp -r bg1920.png ~/.config/
