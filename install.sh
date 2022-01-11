a42abd27fd5670087b1b2578d3eaa6d218b67b17
Francesco Carmelo Capria <leeenzyt@gmail.com>
2022-01-11 11:15:35 +0100

    final updates

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ cp -r alacritty ~/.config/
 cp -r dunst ~/.config/
 cp -r emacs.d ~/
 cp -r i3 ~/.config/
-cp -r lock.sh ~/.config/
+cp -r scripts/lock.sh ~/.config/
 cp -r neofetch ~/.config/
 cp -r picom ~/.config/
 cp -r polybar ~/.config/
 cp -r rofi ~/.config/
-cp AppleColorEmoji.ttf ~/.fonts/
-cp -r bg1920.png ~/.config/
+cp useful/AppleColorEmoji.ttf ~/.fonts/
+cp -r useful/bg1920.png ~/.config/
+
+echo -n "Check the useful dir for other stuffs!"
